d3fd4ac67b2ed593f1ddc5dbfe8a5da6df01bec7
Italo <italobarros@alu.ufc.br>
2019-04-24 11:56:48 -0300

    Configurando ambiente de desenvolvimento e início das primeiras implementações

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Pages extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,13 +18,14 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
-	{
-		$this->load->view('alomundo');
-	}
+		public function index(){
+			$this->load->view('templates/header');
+			$this->load->view('templates/nav');
+			$this->load->view('pages/home');
+			$this->load->view('templates/footer');
+		} 
 
-	public function workshop()
-	{
-		$this->load->view('work');
-	}
+		public function workshop(){
+			$this->load->view('pages/work');
+		}
 }
