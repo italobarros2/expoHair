29604b986a4c76c5b2db9338f511fa0627a60cad
Italo <italobarros@alu.ufc.br>
2019-04-29 22:15:15 -0300

    Povoando a tela principal com os elementos necessários

--- application/views/admin/admin-home.php
+++ application/views/admin/admin-home.php
@@ -1,31 +1,3 @@
-<!-- ============================================================== -->
-<!-- wrapper  -->
-<!-- ============================================================== -->
-<div class="dashboard-wrapper">
-    <div class="dashboard-ecommerce">
-        <div class="container-fluid dashboard-content ">
-            <!-- ============================================================== -->
-            <!-- pageheader  -->
-            <!-- ============================================================== -->
-            <div class="row">
-                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                    <div class="page-header">
-                        <h2 class="pageheader-title">E-commerce Dashboard Template </h2>
-                        <p class="pageheader-text">Nulla euismod urna eros, sit amet scelerisque torton lectus vel mauris facilisis faucibus at enim quis massa lobortis rutrum.</p>
-                        <div class="page-breadcrumb">
-                            <nav aria-label="breadcrumb">
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Dashboard</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">E-Commerce Dashboard Template</li>
-                                </ol>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- ============================================================== -->
-            <!-- end pageheader  -->
-            <!-- ============================================================== -->
             <div class="ecommerce-widget">
                 <div class="row">                    
                     <!-- ============================================================== -->
@@ -33,8 +5,8 @@
                     <!-- ============================================================== -->
                     <div class="col-xl-12 col-lg-12 col-md-6 col-sm-12 col-12">
                         <div class="card">
-                            <h5 class="card-header">Recent Orders</h5>
-                            <div class="card-body p-0">
+                            <h5 class="card-header">Tabela de Transações</h5>
+                            <div class="card-body">
                                 <div class="table-responsive">
                                     <table class="table" id="tabelatest">
                                         <thead class="bg-light">
@@ -51,66 +23,14 @@
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <td>1</td>
-                                                <td>
-                                                    <div class="m-r-10"><img src="assets/images/product-pic.jpg" alt="user" class="rounded" width="45"></div>
-                                                </td>
-                                                <td>Product #1 </td>
-                                                <td>id000001 </td>
-                                                <td>20</td>
-                                                <td>$80.00</td>
-                                                <td>27-08-2018 01:22:12</td>
-                                                <td>Patricia J. King </td>
-                                                <td><span class="badge-dot badge-brand mr-1"></span>InTransit </td>
-                                            </tr>
-                                            <tr>
-                                                <td>2</td>
-                                                <td>
-                                                    <div class="m-r-10"><img src="assets/images/product-pic-2.jpg" alt="user" class="rounded" width="45"></div>
-                                                </td>
-                                                <td>Product #2 </td>
-                                                <td>id000002 </td>
-                                                <td>12</td>
-                                                <td>$180.00</td>
-                                                <td>25-08-2018 21:12:56</td>
-                                                <td>Rachel J. Wicker </td>
-                                                <td><span class="badge-dot badge-success mr-1"></span>Delivered </td>
-                                            </tr>
-                                            <tr>
-                                                <td>3</td>
-                                                <td>
-                                                    <div class="m-r-10"><img src="assets/images/product-pic-3.jpg" alt="user" class="rounded" width="45"></div>
-                                                </td>
-                                                <td>Product #3 </td>
-                                                <td>id000003 </td>
-                                                <td>23</td>
-                                                <td>$820.00</td>
-                                                <td>24-08-2018 14:12:77</td>
-                                                <td>Michael K. Ledford </td>
-                                                <td><span class="badge-dot badge-success mr-1"></span>Delivered </td>
-                                            </tr>
-                                            <tr>
-                                                <td>4</td>
-                                                <td>
-                                                    <div class="m-r-10"><img src="assets/images/product-pic-4.jpg" alt="user" class="rounded" width="45"></div>
-                                                </td>
-                                                <td>Product #4 </td>
-                                                <td>id000004 </td>
-                                                <td>34</td>
-                                                <td>$340.00</td>
-                                                <td>23-08-2018 09:12:35</td>
-                                                <td>Michael K. Ledford </td>
-                                                <td><span class="badge-dot badge-success mr-1"></span>Delivered </td>
-                                            </tr>
-                                           <?php for($i = 0; $i<50; $i++){
+                                           <?php for($i = 0; $i<100; $i++){
                                                 echo'<tr>
                                                         <td>4</td>
                                                         <td>
                                                             <div class="m-r-10"><img src="assets/images/product-pic-4.jpg" alt="user" class="rounded" width="45"></div>
                                                         </td>
-                                                        <td>Product #4 </td>
-                                                        <td>id000004 </td>
+                                                        <td>Product #'.$i.' </td>
+                                                        <td><a href="'.base_url('gerenciamento/compra/'.$i).'">000'.$i.'</a></td>
                                                         <td>34</td>
                                                         <td>$340.00</td>
                                                         <td>23-08-2018 09:12:35</td>
@@ -138,9 +58,9 @@
                     <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
                         <div class="card border-3 border-top border-top-primary">
                             <div class="card-body">
-                                <h5 class="text-muted">Sales</h5>
+                                <h5 class="text-muted">Vendas Online</h5>
                                 <div class="metric-value d-inline-block">
-                                    <h1 class="mb-1">$12099</h1>
+                                    <h1 class="mb-1">R$12099</h1>
                                 </div>
                                 <div class="metric-label d-inline-block float-right text-success font-weight-bold">
                                     <span class="icon-circle-small icon-box-xs text-success bg-success-light"><i class="fa fa-fw fa-arrow-up"></i></span><span class="ml-1">5.86%</span>
@@ -157,9 +77,9 @@
                     <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
                         <div class="card border-3 border-top border-top-primary">
                             <div class="card-body">
-                                <h5 class="text-muted">New Customer</h5>
+                                <h5 class="text-muted">TecHHub</h5>
                                 <div class="metric-value d-inline-block">
-                                    <h1 class="mb-1">1245</h1>
+                                    <h1 class="mb-1">R$1245</h1>
                                 </div>
                                 <div class="metric-label d-inline-block float-right text-success font-weight-bold">
                                     <span class="icon-circle-small icon-box-xs text-success bg-success-light"><i class="fa fa-fw fa-arrow-up"></i></span><span class="ml-1">10%</span>
@@ -176,12 +96,12 @@
                     <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
                         <div class="card border-3 border-top border-top-primary">
                             <div class="card-body">
-                                <h5 class="text-muted">Visitor</h5>
+                                <h5 class="text-muted">Comissões</h5>
                                 <div class="metric-value d-inline-block">
-                                    <h1 class="mb-1">13000</h1>
+                                    <h1 class="mb-1">R$13000</h1>
                                 </div>
-                                <div class="metric-label d-inline-block float-right text-success font-weight-bold">
-                                    <span class="icon-circle-small icon-box-xs text-success bg-success-light"><i class="fa fa-fw fa-arrow-up"></i></span><span class="ml-1">5%</span>
+                                <div class="metric-label d-inline-block float-right text-danger font-weight-bold">
+                                    <span class="icon-circle-small icon-box-xs text-danger bg-danger-light bg-danger-light "><i class="fa fa-fw fa-arrow-down"></i></span><span class="ml-1">4%</span>
                                 </div>
                             </div>
                         </div>
@@ -195,12 +115,12 @@
                     <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
                         <div class="card border-3 border-top border-top-primary">
                             <div class="card-body">
-                                <h5 class="text-muted">Total Orders</h5>
+                                <h5 class="text-muted">Total</h5>
                                 <div class="metric-value d-inline-block">
-                                    <h1 class="mb-1">1340</h1>
+                                    <h1 class="mb-1">R$1340</h1>
                                 </div>
-                                <div class="metric-label d-inline-block float-right text-danger font-weight-bold">
-                                    <span class="icon-circle-small icon-box-xs text-danger bg-danger-light bg-danger-light "><i class="fa fa-fw fa-arrow-down"></i></span><span class="ml-1">4%</span>
+                                <div class="metric-label d-inline-block float-right text-success font-weight-bold">
+                                    <span class="icon-circle-small icon-box-xs text-success bg-success-light"><i class="fa fa-fw fa-arrow-up"></i></span><span class="ml-1">5%</span>
                                 </div>
                             </div>
                         </div>
@@ -221,9 +141,23 @@
                     <!-- ============================================================== -->
                     <div class="col-xl-5 col-lg-5 col-md-12 col-sm-12 col-12">
                         <div class="card">
-                            <h5 class="card-header">Revenue by Category</h5>
+                            <h5 class="card-header">Vendas por categoria</h5>
                             <div class="card-body">
-                                <div id="c3chart_category" style="height: 420px;"></div>
+                                <div id="c3chart_categoria" style="height: 420px;">
+                                    <?php 
+                                        $count = 5;
+                                        $lista = array('workshop_1', 'palestra_5', 'curso_4', 'show_sexta', 'show_sabado');
+                                        echo '<span id="contador" class="display_none">'.$count.'</span>';
+                                        for ($i=1; $i < ($count+1); $i++) { 
+                                            $fator = $i*7;
+                                            echo '
+                                                <span id="elemento-'.$i.'" class="display_none">'.$lista[$i-1].'</span>
+                                                <span id="elemento-'.(2*($count+1)-$i).'" class="display_none">'.$fator.'</span>
+                                            ';
+                                        }
+
+                                    ?>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -233,9 +167,34 @@
 
                     <div class="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-12">
                         <div class="card">
-                            <h5 class="card-header"> Total Revenue</h5>
+                            <h5 class="card-header"> Evolução das vendas</h5>
                             <div class="card-body">
-                                <div id="morris_totalrevenue"></div>
+                                <div id="morris_evolucao_vendas">
+
+                                <?php 
+                                        $meses = array('Maio', 'Junho', 'Julho');
+                                        $controle = 1;
+                                        $lista = array();
+                                        echo '<span id="controle" class="display_none">'.$controle.'</span>';
+
+                                        if($controle = 0){
+                                            for($i = 0; $i< 30; $i++){
+                                                array_push($lista, rand(0, 50));
+                                            }
+                                        }else{
+                                            for($i = 0; $i< 31; $i++){
+                                                array_push($lista, rand(0,50));
+                                            }
+                                        }
+                                        for ($i=0; $i < sizeof($lista); $i++) { 
+                                            echo '
+                                                <span id="elemento-morris-'.$i.'" class="display_none">'.$lista[$i].'</span>
+                                            ';
+                                        }
+
+                                ?>
+
+                                </div>
                             </div>
                             <div class="card-footer">
                                 <p class="display-7 font-weight-bold"><span class="text-primary d-inline-block">$26,000</span><span class="text-success float-right">+9.45%</span></p>
