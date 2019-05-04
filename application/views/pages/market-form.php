d0e1ca9b6500a0243bfb5c1c856094ff14b46d7b
Italo <italobarros@alu.ufc.br>
2019-05-04 12:16:19 -0300

    Adicionando página de comissões

---
+++ application/views/pages/market-form.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ítalo
+ * Date: 4/29/2019
+ * Time: 11:48 PM
+ */
---
+++ application/views/templates/nav-market.php
@@ -0,0 +1,31 @@
+<!-- Page Preloder -->
+<div id="preloder">
+    <div class="loader"></div>
+</div>
+	
+	<!-- Header section -->
+<header id="header" class="header-section">
+		<div class="header-warp">
+			<!-- logo -->
+			<a href="index.php" class="site-logo">
+				<img src="<?=base_url('static/img/logo.png')?>" width="250px" alt="">
+			</a>
+			<!-- responsive -->
+			<div class="nav-switch">
+				<i class="fa fa-bars"></i>
+			</div>
+			<!-- Navigation Menu -->
+			<ul class="main-menu">
+				<li class="active"><a href="/">Home</a></li>
+				<li><a href="#">Cursos/Palestras/Workshops</a></li>	
+				<li><a href="sobre.php">Sobre</a></li>
+				<li><a href="#">Contato</a></li>
+				<li><a href="mapa.php">Planta</a></li>
+			</ul>
+			<div class="header-right">
+				<a href="#" class="site-btn sb-line sb-big">(85) 9 9659 - 7500</a>
+				<a href="#" class="site-btn sb-big">Inscrições</a>
+			</div>
+		</div>
+</header>
+	<!-- Header section end -->
